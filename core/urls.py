719fc00eb0fa43a3972d3c84b328eac6268cd1be
Muhammad <explogamer43@gmail.com>
2026-01-01 23:00:51 +0100

    Add contact and about views

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.home, name='home'), # Home page
+    path('about/', views.about, name='about'), # About page
+    path('contact/', views.contact, name='contact'), # Contact page
     ]
