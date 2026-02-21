3cb0ee7148b982291bf4ce0b81734c0986fff34f
Explogame <explogamer43@gmail.com>
2026-02-21 16:52:24 +0100

    Preparing for render deployment

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     
     'apps.core',
     'apps.quiz',
@@ -122,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_DIR = [
     BASE_DIR/"static",
 ]
