22aa489c71240b6a29a8d647d6aef6a31885124d
Explogame <explogamer43@gmail.com>
2026-02-22 09:56:47 +0100

    Update prod.py Databases

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,7 +7,11 @@ DEBUG = False
 ALLOWED_HOSTS = ['.onrender.com']
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
