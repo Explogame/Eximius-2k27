28f3be816b22e737caa1303109d5a80291b6ab46
Explogame <explogamer43@gmail.com>
2026-02-22 10:53:20 +0100

    Update core apps.py

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,22 +1,28 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 import os
+
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.core'
 
     def ready(self):
-        User = get_user_model()
+        from django.db.models.signals import post_migrate
+        post_migrate.connect(create_superuser, sender=self)
 
-        admin_email = os.getenv("ADMIN_EMAIL")
-        admin_username = os.getenv("ADMIN_USERNAME")
-        admin_password = os.getenv("ADMIN_PASSWORD")
 
-        if admin_email and admin_username and admin_password:
-            if not User.objects.filter(username=admin_username).exists():
-                User.objects.create_superuser(
-                    username=admin_username,
-                    email=admin_email,
-                    password=admin_password
-                )
+def create_superuser(sender, **kwargs):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    admin_email = os.getenv("ADMIN_EMAIL")
+    admin_username = os.getenv("ADMIN_USERNAME")
+    admin_password = os.getenv("ADMIN_PASSWORD")
+
+    if admin_email and admin_username and admin_password:
+        if not User.objects.filter(username=admin_username).exists():
+            User.objects.create_superuser(
+                username=admin_username,
+                email=admin_email,
+                password=admin_password
+            )
