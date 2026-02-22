e81518f6db96f0cab9c844af42c4ad3025402d8a
Explogame <explogamer43@gmail.com>
2026-02-22 10:44:18 +0100

    Update settings folder

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,10 +1,10 @@
 from .base import *
-import dj_database_url
 import os
-
+import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ['.onrender.com']
+
+ALLOWED_HOSTS = [".onrender.com"]
 
 DATABASES = {
     "default": dj_database_url.config(
@@ -14,31 +14,8 @@ DATABASES = {
     )
 }
 
-'''
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
+CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com"]
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# Dashboard cache
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'eximius-cache',
-    }
-}
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {'class': 'logging.StreamHandler'},
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
-}
-'''
